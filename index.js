8ad47efb9575cdb92a91d5977b7e51f7e0b3c31f
Satish K Yadav (सतीश के यादव) <satishkyadav@corp.bharatpetroleum.com>
2018-10-20 12:55:40 +0530

    Theme updated to dark-classy-responsive

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
  
 var fs = require('fs');
 var path = require('path');
-var eloquent = require('jsonresume-theme-eloquent');
+// var theme = require('jsonresume-theme-eloquent');
+var theme = require('jsonresume-theme-dark-classy-responsive');
  
 var resumeJSON = JSON.parse(fs.readFileSync(path.join(__dirname, '/resume.json'), 'utf-8'));
-var resumeHTML = eloquent.render(resumeJSON);
+var resumeHTML = theme.render(resumeJSON);
  
 fs.writeFile(path.join(__dirname, '/resume.html'), resumeHTML, function(err) {
     console.log(err || 'Resume exported to resume.html');
