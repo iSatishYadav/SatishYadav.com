bad580d4bfe12d58b49f64472e7a49edc381d2f2
Satish Yadav <monstersatish@live.com>
2018-11-01 21:40:18 +0530

    Copying html to index.html

--- index.js
+++ index.js
@@ -11,3 +11,6 @@ var resumeHTML = theme.render(resumeJSON);
 fs.writeFile(path.join(__dirname, '/resume.html'), resumeHTML, function(err) {
     console.log(err || 'Resume exported to resume.html');
 });
+fs.writeFile(path.join(__dirname, '/index.html'), resumeHTML, function(err) {
+    console.log(err || 'Resume exported to resume.html');
+});
